b8e2235cd98049f9cd670a6eb4006e68df08dd98
Sovanna <sovanna.hing@gmail.com>
2013-10-26 19:18:23 +0200

    Initial Commit

---
+++ SHAppLib/SHAppLib.h
@@ -0,0 +1,13 @@
+//
+//  SHAppLib.h
+//  SHAppLib
+//
+//  Created by Sovanna Hing on 26/10/2013.
+//  Copyright (c) 2013 Sovanna Hing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHAppLib : NSObject
+
+@end
---
+++ SHAppLib/SHAppLib.m
@@ -0,0 +1,13 @@
+//
+//  SHAppLib.m
+//  SHAppLib
+//
+//  Created by Sovanna Hing on 26/10/2013.
+//  Copyright (c) 2013 Sovanna Hing. All rights reserved.
+//
+
+#import "SHAppLib.h"
+
+@implementation SHAppLib
+
+@end
